b95ad489870f26bc7a2be002a94f485bd0d8fd21
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-26 09:13:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import quizService from "./quizService";
 import QuestionBox from "./components/QuestionBox";
 class QuizBee extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () =>  {
         quizService().then(question => {
@@ -14,6 +16,16 @@ class QuizBee extends Component {
             });
         });
     };
+    computeAnser = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+    }
     componentDidMount(){
      this.getQuestions();
     }
@@ -23,6 +35,8 @@ class QuizBee extends Component {
             <div className="title"> 
             QuizBee
             </div>
+            {this.state.questionBank.length > 0 &&
+            this.state.responses < 5 }
             {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => 
             (<QuestionBox question={question} options={answers} key={questionId} selected={answer => this.computeAnswer(answer,correct)}/>
             )
