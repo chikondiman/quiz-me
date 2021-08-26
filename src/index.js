6387fb06e6be5e1b1ba647a6ddf0ed2f5e15632e
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-08-26 05:22:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React, { Component } from "react";
 import ".assets/style.css";
+import ReactDOM from "react-dom";
+class QuizBee extends Component {
+    render() {
+        return (
+            <div className="container">
+            <div className="title"> 
+            QuizBee
+            </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<QuizBee/>, document.getElementById("root"));
 
